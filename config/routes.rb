c7c0614751a071bc456973e97e2e62dd8b2a26a6
Nathan Baggs <nabaggs.2013@mymail.becker.edu>
2016-10-19 10:58:49 -0400

    Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  get 'storefront/all_items'
 
-  get 'storefront/items_by_category'
+  post 'add_to_cart'=>'cart#add_to_cart'
 
-  get 'storefront/items_by_brand'
+  get 'view_order'=>'cart#view_order'
+
+  get 'checkout'=>'cart#checkout'
+
+  resources :line_items
+  resources :orders
+  devise_for :users
+  get 'all'=>'storefront#all_items'
+
+  get 'categorical'=>'storefront#items_by_category'
+
+  get 'branding'=>'storefront#items_by_brand'
 
   resources :products
   resources :categories
